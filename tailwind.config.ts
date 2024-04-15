6ecb0a6710541eaf7fac26d9ed7588464cfd965a
Paige Knowles <paknowles@student.fullsail.edu>
2024-04-15 13:42:38 -0400

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        background: "#363C45",
+        text: "#E4DDC0",
+        headings: "#BFC8B3",
+      },
     },
   },
   plugins: [],
